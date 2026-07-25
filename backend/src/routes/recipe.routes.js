c118e39b6fc01a49c31bf942637bd40372778d55
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-07-25 13:22:17 -0400

    feat(backend): renamed rescipes controller file

--- backend/src/routes/recipe.routes.js
+++ backend/src/routes/recipe.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getRecipes } from '../controllers/getRecipes.controller.js';
+import { getRecipes } from '../controllers/recipe.controller.js';
 
 const router = Router();
 
