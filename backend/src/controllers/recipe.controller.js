03a5f3f826bbfb680d1c72728816df13278d3683
Stephen Lewis <st.rayis1085@gmail.com>
2026-07-28 16:07:03 -0400

    Meal 33 get controller (#6)
    * feat(backebnd): feat imported prisma pool
    
    * feat(backend): pagination and search clause created
    
    * feat(backend): search and sort logic implemented for GET. yet to test
    
    * fix(backend):prisma schema needs to be updated locally
    
    * fix(backend): updated prisma client (#5)
    
    Sorry thought i would need permission to merge
    
    * feat(backed): tested and proven. 9 recipes
    
    * Meal 34 post controller (#7)
    
    * feat(backend): normalizing data before putting in the db.
    
    * feat(backend): putting recipe into db after normalizing
    
    * chore(backend): final touches to allow for free flowing work. (optimal success planned for)

--- backend/src/controllers/recipe.controller.js
+++ backend/src/controllers/recipe.controller.js
@@ -1,3 +1,4 @@
+import { prisma } from '../db.js';
 /**
  * We are sending back a hard coded array of recipes
  * so that the front end can continue with develping
@@ -44,6 +45,10 @@ const MOCK_RECIPE_DATA = [
 ];
 
 export async function getRecipes(req, res) {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 9;
+  const skip = (page - 1) * limit;
+
   const whereClause = {};
 
   if (req.query.find) {
@@ -52,27 +57,116 @@ export async function getRecipes(req, res) {
       mode: 'insensitive',
     };
   }
-  /**
-   * TODO: search/filter logic
-   *       db query via prisma
-   */
+  let recipes = null;
+  let total = null;
 
-  return res.status(200).json(MOCK_RECIPE_DATA);
+  function getOrderBy(query) {
+    const validSortFields = ['protein', 'carbs', 'fat', 'calories'];
+    const sortBy = query.sortBy || 'created_at';
+    const sortDirection = query.sortDirection === 'asc' ? 'asc' : 'desc';
+
+    if (validSortFields.includes(sortBy)) {
+      return { [sortBy]: sortDirection };
+    }
+
+    return { created_at: sortDirection };
+  }
+
+  try {
+    recipes = await prisma.recipes.findMany({
+      where: whereClause,
+      select: {
+        id: true,
+        instructions: true,
+        ingredients: true,
+        total_time_minutes: true,
+        servings: true,
+        title: true,
+        calories: true,
+        fat: true,
+        protein: true,
+        carbs: true,
+      },
+      skip: skip,
+      take: limit,
+      orderBy: getOrderBy(req.query),
+    });
+
+    total = await prisma.recipes.count({ where: whereClause });
+  } catch (error) {
+    console.log('Error in get catch', error);
+  }
+
+  const pagination = {
+    page,
+    limit,
+    total,
+    pages: Math.ceil(total / limit),
+    hasNext: page * limit < total,
+    hasPrev: page > 1,
+  };
+
+  if (recipes.length === 0) {
+    return res.status(404).json({
+      error: 'No recipes could be found',
+      message: 'No recipes meet the search criteria',
+    });
+  }
+
+  return res.status(200).json({ recipes, pagination });
 }
 
 export async function createRecipe(req, res) {
-  const { title, instructions, ingredients, servings, total_time_minutes } =
-    req.body;
+  // if (!req.user.id) {
+  //   return res
+  //     .status(404)
+  //     .json({ error: 'Bad Request', message: 'Need a user' });
+  // }
 
-  return res.status(201).json({
-    title,
-    instructions,
-    total_time_minutes,
-    servings,
-    ingredients,
-    calories: 134,
-    protein: 9,
-    fat: 10,
-    carbs: 0,
-  });
+  //This will be replaced with validations later
+  const cleanedData = normalizeData(req.body);
+
+  // cleanedData.user_id = req?.user?.id;
+  cleanedData.user_id = 1;
+
+  let newRecipeCreated = null;
+  try {
+    newRecipeCreated = await prisma.recipes.create({
+      data: cleanedData,
+      select: {
+        id: true,
+        instructions: true,
+        ingredients: true,
+        total_time_minutes: true,
+        servings: true,
+        title: true,
+        calories: true,
+        fat: true,
+        protein: true,
+        carbs: true,
+      },
+    });
+    console.log('Marker hit without error', newRecipeCreated);
+  } catch (error) {
+    console.log('Create Recipe catch', error);
+    res.status(500).json({
+      error: error.message,
+      message: 'Server/Database Connection Error',
+    });
+  }
+  // return res.status(201).json(newRecipeCreated);
+  return res.status(201).json(newRecipeCreated);
 }
+
+const normalizeData = (reqBody) => {
+  const holder = {};
+  for (let propName in reqBody) {
+    const value = reqBody[propName];
+    if (isNaN(value)) {
+      holder[propName] = value.trim();
+    } else {
+      holder[propName] = Math.trunc(value);
+    }
+  }
+  return holder;
+};
