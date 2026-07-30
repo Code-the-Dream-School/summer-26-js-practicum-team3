c47b22d2f6888e7e8348fea7b1c6a83c1728c63c
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-07-29 20:04:44 -0400

    chore(frontend): basic styles

--- frontend/src/pages/AddRecipe/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe/AddRecipe.jsx
@@ -3,7 +3,45 @@ import styles from './AddRecipe.module.css';
 export default function AddRecipe() {
   return (
     <div className={styles.root}>
-      <h2>Hello world</h2>
+      <h2>Add Recipe</h2>
+      <form className={styles.formContainer}>
+        <div>
+          <label htmlFor="title">Recipe Name</label>
+          <input id="title" type="text" required />
+        </div>
+        <div>
+          <label htmlFor="instructions">Instructions</label>
+          <input id="instructions" type="text" required />
+        </div>
+        <div>
+          <label htmlFor="ingredients">Ingredients</label>
+          <input id="ingredients" type="text" />
+        </div>
+        <div>
+          <label htmlFor="total_time">Total cook time</label>
+          <input id="total_time" type="text" inputMode="numeric" />
+        </div>
+        <div>
+          <label htmlFor="servings">Servings</label>
+          <input id="servings" type="text" inputMode="numeric" />
+        </div>
+        <div>
+          <label htmlFor="calories">Calories</label>
+          <input id="calories" type="text" inputMode="numeric" />
+        </div>
+        <div>
+          <label htmlFor="protein">Protein</label>
+          <input id="protein" type="text" inputMode="numeric" />
+        </div>
+        <div>
+          <label htmlFor="carbs">Carbs</label>
+          <input id="carbs" type="text" inputMode="numeric" />
+        </div>
+        <div>
+          <label htmlFor="fats">Fats</label>
+          <input id="fats" type="text" inputMode="numeric" />
+        </div>
+      </form>
     </div>
   );
 }
