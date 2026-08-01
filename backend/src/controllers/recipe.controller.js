72ef196fbbeb779e9d021df68a3c288a63e5bd13
Stephen Lewis <13525251+mxmason@users.noreply.github.com>
2026-08-01 14:44:15 -0400

    Meal 52 delete api endpoint (#19)
    * lewis/feat/backend: stubbed delete route and controller
    
    * lewis/feat/backend: delete logic implemented
    
    * lewis/fix/backend: typo

--- backend/src/controllers/recipe.controller.js
+++ backend/src/controllers/recipe.controller.js
@@ -208,6 +208,32 @@ export async function updateRecipe(req, res, next) {
   return;
 }
 
+export async function deleteRecipe(req, res) {
+  const recipeIndex = parseInt(req.params?.id);
+  // const user_id = req.user.id;
+  const user_id = 1;
+
+  if ((recipeIndex < 0) | (user_id < 0)) {
+    res.status(400).json({ message: 'Validation Error', error: 'invalid id' });
+    return;
+  }
+
+  try {
+    await prisma.recipes.delete({
+      where: {
+        id: recipeIndex,
+        user_id: user_id,
+      },
+    });
+  } catch (error) {
+    console.log('Update Recipe Catch', error);
+    res.status(400).json({ message: 'Prisma Error', error: error.message });
+    return;
+  }
+
+  res.status(204).end();
+}
+
 const normalizeData = (reqBody) => {
   const holder = {};
   for (let propName in reqBody) {
--- backend/src/routes/recipe.routes.js
+++ backend/src/routes/recipe.routes.js
@@ -3,12 +3,12 @@ import {
   getRecipes,
   createRecipe,
   updateRecipe,
+  deleteRecipe,
 } from '../controllers/recipe.controller.js';
 
 const router = Router();
 
-router.get('/', getRecipes);
-router.post('/', createRecipe);
-router.patch('/:id', updateRecipe);
+router.get('/', getRecipes).post('/', createRecipe);
+router.patch('/:id', updateRecipe).delete('/:id', deleteRecipe);
 
 export default router;
