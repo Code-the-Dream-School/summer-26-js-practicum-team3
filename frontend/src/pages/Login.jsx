ba18fe5a42c68c3f4223031f2e729a5602e0c685
terriberri82 <102176320+terriberri82@users.noreply.github.com>
2026-08-06 18:49:59 -0400

    Meal 63 material UI (#34)
    * feat(frontend): add signup and login routes (MEAL-61)
    
    * feat(frontend): style signup  page with Material UI (MEAL-63)
    
    ---------
    
    Co-authored-by: Stephen Lewis <st.rayis1085@gmail.com>

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router';
 
+
 function Login() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+    <div style={{ padding: '2rem'}}>
       <h2>Log In</h2>
       <p>
         Don't have an account? <Link to="/signup">Sign Up</Link>
       </p>
+      <Button variant="contained">Test</Button>
     </div>
   );
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,14 +1,27 @@
 import { Link } from 'react-router';
 import SignUpForm from '../features/auth/components/SignUpForm';
+import { Card, Box, CardContent, Typography } from '@mui/material';
 
 function SignUp() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <p>
-        Have an account? <Link to="/login">Log In</Link>
-      </p>
-      <SignUpForm />
-    </div>
+    <Box sx={{ maxWidth: 480, mx: 'auto', p: 3 }}>
+      <Card variant="outlined" sx={{ borderRadius: 3 }}>
+        <CardContent sx={{ p: 4 }}>
+          <Typography
+            variant="h4"
+            fontWeight={600}
+            align="center"
+            sx={{ mb: 3 }}
+          >
+            Sign up
+          </Typography>
+          <SignUpForm />
+          <Typography variant="body1" align="center" sx={{ mt: 3 }}>
+            Have an account? <Link to="/login">Log In</Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
