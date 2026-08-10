8b4791662ffcabe3f0ee080576ffea198aabff40
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-10 14:20:02 -0400

    lewis/feat/backend: git files changed

--- .husky/scripts/check_pull_changes.js
+++ .husky/scripts/check_pull_changes.js
@@ -1,10 +1,16 @@
 import { execSync } from 'child_process';
 
+// if git diff-tree has the file names
+// then those files have been changed
+// files changed = true ? generate db client and/or
+// installing node packages:null;
+const GIT_CHANGED_FILES =
+  'git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD';
 try {
-  // if git diff-tree has the file names
-  // then those files have been changed
-  // files changed = true ? generate db client and/or
-  // installing node packages:null;
+  const changed_files = execSync(GIT_CHANGED_FILES)
+    .toString()
+    .trim()
+    .split('\n');
 } catch (error) {
   console.log('Error in checking files changed \n', error.message);
   process.exit(1);
