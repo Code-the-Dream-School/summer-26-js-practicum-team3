b4ecba17044756802a6b1c250a18a25923dda567
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-13 21:04:38 -0400

    lewis/feat/frontend: pagination through pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,52 +1,15 @@
 import { RecipeCard } from '../components/RecipeCard.jsx';
 import { useEffect, useState } from 'react';
-
-// TODO: Replace MOCK_RECIPE_DATA with data fetched from the backend API
-const MOCK_RECIPE_DATA = [
-  {
-    title: 'How to Cook Bacon in the Oven',
-    instructions: 'https://www.allrecipes.com/recipe/267904/oven-baked-bacon',
-    total_time_minutes: 35,
-    servings: 6,
-    calories: 134,
-    protein: 9,
-    fat: 10,
-    carbs: 0,
-    ingredients: '1 (16 ounce) package bacon',
-  },
-  {
-    title: 'Boiled Peanuts',
-    instructions: 'https://www.allrecipes.com/recipe/17551/boiled-peanuts/',
-    total_time_minutes: 185,
-    servings: 40,
-    calories: 322,
-    protein: 15,
-    fat: 28,
-    carbs: 9,
-    ingredients:
-      '5 pounds raw peanuts, in shells, 1 cup salt, or to taste, water to cover',
-  },
-  {
-    id: 967,
-    user_id: 1,
-    title: 'Manicotti Pancakes II',
-    instructions:
-      'https://www.allrecipes.com/recipe/20566/manicotti-pancakes-ii/',
-    total_time_minutes: 15,
-    servings: 12,
-    calories: 66,
-    protein: 3,
-    fat: 2,
-    carbs: 9,
-    ingredients: '3  eggs, 1 cup milk, 1 cup all-purpose flour',
-  },
-];
+import { paginationFetch } from '../utils/api-helper.js';
 
 export default function Home() {
   const [recipes, setRecipes] = useState([]);
   const [message, setMessage] = useState('');
   const [error, setError] = useState(null);
-
+  const [pageLimit, setPageLimit] = useState(8);
+  const [databasepageNumber, setDatabasePageNumber] = useState(1);
+  const [page, setPage] = useState({});
+  console.log('top level', recipes);
   useEffect(() => {
     let stopDoubles = false;
 
@@ -64,15 +27,14 @@ export default function Home() {
         data = await resp.json();
         console.log(data.recipes);
         if (!stopDoubles) {
-          setRecipes((previous) => [...previous, ...data.recipes]);
+          setRecipes(data.recipes);
+          // setRecipes((previous) => [...previous, ...data.recipes]);
         }
       } catch (error) {
         setError(error.message);
       }
     }
-
     baseFetch();
-
     return () => {
       console.log('one render clean-up');
       stopDoubles = true;
@@ -80,6 +42,17 @@ export default function Home() {
   }, []);
   // TODO: dashboard
   // return <Dashboard />;
+
+  async function getMore() {
+    const nextPageNumber = databasepageNumber + 1;
+    setDatabasePageNumber(nextPageNumber);
+    console.log('something', databasepageNumber);
+    const nextSetOfRecipes = await paginationFetch(
+      `http://localhost:8080/api/v1/recipes/?page=${nextPageNumber}`,
+    );
+    setRecipes(nextSetOfRecipes.recipes);
+    setPage(nextSetOfRecipes.pagination);
+  }
 
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
@@ -97,6 +70,9 @@ export default function Home() {
           <RecipeCard key={index} {...recipe} />
         ))}
       </div>
+      <button type="button" onClick={getMore}>
+        Get More
+      </button>
     </main>
   );
 }
