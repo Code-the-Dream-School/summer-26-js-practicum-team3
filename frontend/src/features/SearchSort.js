f749e283d30b22b99fa7f1959794a347100fcc0a
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-14 13:46:05 -0400

    lewis/feat/backend: filtering and direction of macros

---
+++ frontend/src/features/SearchSort.js
@@ -0,0 +1,37 @@
+/*eslint-disable react/prop-types */
+export default function SortBy({
+  sortBy,
+  sortDirection,
+  onSortByChange,
+  onSortDirectionChange,
+}) {
+  return (
+    <form>
+      <label htmlFor="sortBy">Sort By: </label>
+      <select
+        aria-label="sort by"
+        name="sortBy"
+        id="sortBy"
+        value={sortBy}
+        onChange={(e) => onSortByChange(e.target.value)}
+      >
+        <option value="calories">Calories</option>
+        <option value="carbs">Carbs</option>
+        <option value="fats">Fats</option>
+        <option value="protein">Protein</option>
+      </select>
+
+      <label htmlFor="order">Sorting Order: </label>
+      <select
+        arai-label="sort direction"
+        value={sortDirection}
+        onChange={(e) => onSortDirectionChange(e.target.value)}
+        name="order"
+        id="order"
+      >
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
+      </select>
+    </form>
+  );
+}
