43df2cf506a79ddf5bbae1b73b70bc3e6a33c2f6
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-19 14:47:14 -0400

    lewis/chore/frontend: styled search bar with MUI

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -1,15 +1,26 @@
 /* eslint-disable react/prop-types */
+import { FormControl, TextField } from '@mui/material';
+const SEARCH_CONTAINER = {
+  width: '95%',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+};
+const SEARCH_BAR = {
+  width: '50%',
+  alignText: 'center',
+};
 export function SearchInput({ searchTerm, onFilterChange }) {
   return (
-    <form>
-      <label htmlFor="filterInput">Search Recipes: </label>
-      <input
+    <FormControl sx={SEARCH_CONTAINER}>
+      <TextField
+        sx={SEARCH_BAR}
+        variant="standard"
+        label="Search Recipes"
         id="filterInput"
-        type="text"
         value={searchTerm}
         onChange={(e) => onFilterChange(e.target.value)}
-        placeholder="search todos here..."
       />
-    </form>
+    </FormControl>
   );
 }
