10468fed5528b8930f3bdbc20028097fbe0d7a97
XavierCTD <macfamlove01@gmail.com>
2026-08-21 19:10:41 -0400

    MEAL-128-chore(frontend): Created a contact page.

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <>
+      <div style={{ padding: '2rem' }}>
+        <h2>Contact Page</h2>
+        <p>This is the contact page. This is a WIP.</p>
+      </div>
+    </>
+  );
+}
