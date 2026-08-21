d7848e7f69b2aee63e14028981e9cfc64a3d326a
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-21 12:48:30 -0400

    lewis/feat/frontend: Created and implement onboarding reminder flog on front end

--- frontend/src/components/OnboardingFlag.jsx
+++ frontend/src/components/OnboardingFlag.jsx
@@ -1,9 +1,18 @@
-import { LabelImportantIcon } from '@mui/icons-material';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
 import { Typography, Box } from '@mui/material';
+import { Link } from 'react-router';
+
+const ONBOARDING_FLAG = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textDecoration: 'none',
+};
 
 export function OnboardingFlag() {
   return (
-    <Box>
+    <Box component={Link} to="/onboarding" sx={ONBOARDING_FLAG}>
       <Typography>Finish</Typography>
       <LabelImportantIcon />
       <Typography>Onboarding</Typography>
