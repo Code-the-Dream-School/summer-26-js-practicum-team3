f4535f925337300abdc4ac01f94bdffae066d78d
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-22 13:12:07 -0400

    lewis/chore/frontend: cleaned up unwanted code.

--- frontend/src/components/OnboardingFlag.jsx
+++ frontend/src/components/OnboardingFlag.jsx
@@ -1,21 +1,10 @@
-import LabelImportantIcon from '@mui/icons-material/LabelImportant';
-import { Typography, Box } from '@mui/material';
+import { Button } from '@mui/material';
 import { Link } from 'react-router';
-
-const ONBOARDING_FLAG = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  textDecoration: 'none',
-};
 
 export function OnboardingFlag() {
   return (
-    <Box component={Link} to="/onboarding" sx={ONBOARDING_FLAG}>
-      <Typography>Finish</Typography>
-      <LabelImportantIcon />
-      <Typography>Onboarding</Typography>
-    </Box>
+    <Button component={Link} to="/onboarding" color="inherit">
+      Finish Onboarding
+    </Button>
   );
 }
