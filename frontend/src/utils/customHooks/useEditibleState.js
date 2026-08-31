6a2a5256567984c5b3911ee46f28aca5272193e8
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-08-31 18:58:36 -0400

    lewis/feat/frontend: created custom hook to edit profile

---
+++ frontend/src/utils/customHooks/useEditibleState.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+export function useEditableState(initialTitle) {
+  const [workingTitle, setWorkingTitle] = useState(initialTitle);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function startEditing() {
+    setWorkingTitle(initialTitle);
+    setIsEditing(true);
+  }
+
+  function cancelEdit() {
+    setWorkingTitle(initialTitle);
+    setIsEditing(false);
+  }
+
+  function updateTitle(newTitle) {
+    setWorkingTitle(newTitle);
+  }
+
+  function finishEdit() {
+    setIsEditing(false);
+    return workingTitle;
+  }
+
+  return {
+    isEditing,
+    workingTitle,
+    startEditing,
+    cancelEdit,
+    updateTitle,
+    finishEdit,
+  };
+}
