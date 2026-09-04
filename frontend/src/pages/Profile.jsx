5927976eb3ab26b9ccf7b030fc780444f52de4b3
Stephen Lewis <st.rayis1085@gmail.com>
2026-09-04 02:06:39 -0400

    Meal 155 profile edit (#97)
    * lewis/feat/frontend: created custom hook to edit profile
    
    * lewis/feat/frontend: making profile page editible.
    
    text fields for the first 3. but because of the limited options we allow into the db, i chose to limit the activity level with a select
    
    * lewis/feat:form updates properly
    
    * lewis/broke: clicking edit refreshes page?
    
    * lewis/feat/frontend: data captured
    
    * fix: add keys to disambiguate elements
    
    * lewis/feat/frontend: altered create nutrition goals to use transaction.
    
    this allows for onboarding to go smoothly and all information being saved and updated
    
    * lewis/chore/backend: UserController updatingOnboardingProfile only writes to users table. renamed and fixed logic for better readablity
    
    * lewis/feat:user profile page saves updatd info
    
    * lewis/feat:user updated joi to take at least 1 change.
    
    * lewis/chore: final pass
    
    * lewis/frontend: adjusted select ot conform to the values needed on the backend
    
    ---------
    
    Co-authored-by: EJ Mason <13525251+mxmason@users.noreply.github.com>

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getProfile } from '../features/auth/api/authApi';
+import { baseFetch } from '../utils/api-helper';
+import { useAuth } from '../features/auth/context/AuthContext';
+
 import {
   Card,
   Box,
@@ -8,16 +11,24 @@ import {
   Grid,
   Divider,
   Button,
+  TextField,
+  MenuItem,
 } from '@mui/material';
 
+const API_ORIGIN = import.meta.env.VITE_API_ORIGIN ?? '';
+const BASE_PATH = `${API_ORIGIN}/api/v1/users/me`;
+
 export default function Profile() {
-  const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const { csrfToken } = useAuth();
 
   useEffect(() => {
     const fetchProfile = async () => {
-      const { status, data } = await getProfile();
+      let { status, data } = await getProfile();
       if (status === 200) {
+        data.dob = data.dob.split('T')[0];
         setProfile(data);
       }
       setLoading(false);
@@ -26,11 +37,42 @@ export default function Profile() {
     fetchProfile();
   }, []);
 
-  if (loading) return <Typography>Loading...</Typography>;
-  if (!profile) return <Typography>Error loading profile.</Typography>;
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const payload = {
+      name: profile.name,
+      email: profile.email,
+      dob: profile.dob,
+      activity_level: profile.activity_level,
+    };
+    try {
+      let data = await baseFetch(BASE_PATH, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-TOKEN': csrfToken,
+        },
+        body: JSON.stringify(payload),
+        credentials: 'include',
+      });
+
+      data.dob = data.dob.split('T')[0];
+      setProfile(() => ({ ...data }));
+      setIsEditing(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <Box sx={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}
+    >
+      {loading && <Typography>Loading...</Typography>}
+      {!profile && <Typography>Error loading profile.</Typography>}
       <Typography variant="h4" sx={{ mb: 3, fontWeight: 'bold' }}>
         My Profile
       </Typography>
@@ -43,45 +85,137 @@ export default function Profile() {
           <Divider sx={{ mb: 2 }} />
 
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid xs={12} sm={6}>
               <Typography variant="subtitle2" color="textSecondary">
                 Name:
               </Typography>
-              <Typography variant="body1">{profile?.name || 'N/A'}</Typography>
+              {isEditing ? (
+                <TextField
+                  onChange={(e) =>
+                    setProfile((previous) => ({
+                      ...previous,
+                      name: e.target.value,
+                    }))
+                  }
+                  variant="standard"
+                  value={profile.name}
+                />
+              ) : (
+                <Typography variant="body1">
+                  {profile?.name || 'N/A'}
+                </Typography>
+              )}
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid xs={12} sm={6}>
               <Typography variant="subtitle2" color="textSecondary">
                 Email:
               </Typography>
-              <Typography variant="body1">{profile?.email || 'N/A'}</Typography>
+              {isEditing ? (
+                <TextField
+                  onChange={(e) =>
+                    setProfile((previous) => ({
+                      ...previous,
+                      email: e.target.value,
+                    }))
+                  }
+                  sx={{ width: '250px' }}
+                  variant="standard"
+                  value={profile.email}
+                />
+              ) : (
+                <Typography variant="body1">
+                  {profile?.email || 'N/A'}
+                </Typography>
+              )}
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid xs={12} sm={6}>
               <Typography variant="subtitle2" color="textSecondary">
-                Sex:
+                Date of Birth:
               </Typography>
-              <Typography variant="body1">
-                {profile?.sex || 'Not provided'}
-              </Typography>
+              {isEditing ? (
+                <TextField
+                  type="date"
+                  onChange={(e) =>
+                    setProfile((previous) => ({
+                      ...previous,
+                      dob: e.target.value,
+                    }))
+                  }
+                  variant="standard"
+                  placeholder="YYYY/MM/DD"
+                />
+              ) : (
+                <Typography variant="body1">
+                  {profile?.dob || 'Not provided'}
+                </Typography>
+              )}
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid xs={12} sm={6}>
               <Typography variant="subtitle2" color="textSecondary">
                 Activity Level:
               </Typography>
-              <Typography variant="body1">
-                {profile?.activity_level || 'Not provided'}
-              </Typography>
+              {isEditing ? (
+                <ActivityLevelOptions
+                  onChange={(value) => {
+                    setProfile((previous) => ({
+                      ...previous,
+                      activity_level: value,
+                    }));
+                  }}
+                  value={profile.activity_level || ''}
+                />
+              ) : (
+                <Typography variant="body1">
+                  {profile?.activity_level || 'Not provided'}
+                </Typography>
+              )}
             </Grid>
           </Grid>
         </CardContent>
       </Card>
       <Box sx={{ display: 'flex', gap: 2 }}>
-        <Button variant="contained" color="primary">
-          Edit Profile
-        </Button>
-        <Button variant="outlined" color="secondary">
+        {isEditing ? (
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!isEditing}
+            key="update-button"
+          >
+            Update
+          </Button>
+        ) : (
+          <Button
+            type="button"
+            onClick={() => setIsEditing(true)}
+            variant="contained"
+            color="primary"
+            key="edit-button"
+          >
+            Edit
+          </Button>
+        )}
+        {/* <Button variant="outlined" color="secondary">
           Change Password
-        </Button>
+        </Button> */}
       </Box>
     </Box>
+  );
+}
+const NO_OP = () => {};
+function ActivityLevelOptions({ value, onChange = NO_OP }) {
+  return (
+    <TextField
+      select
+      sx={{ width: '200px' }}
+      onChange={(e) => onChange(e.target.value)}
+      value={value}
+      variant="standard"
+    >
+      <MenuItem value="sedentary">Sedentary</MenuItem>
+      <MenuItem value="light_activity">Light</MenuItem>
+      <MenuItem value="medium_activity">Medium</MenuItem>
+      <MenuItem value="very-active">Very Active</MenuItem>
+    </TextField>
   );
 }
